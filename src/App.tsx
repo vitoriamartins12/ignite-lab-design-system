f640c3c91119e2fbab6050d20dab02b5c2191e5d
Vitória <vitoria@MBP-de-Rodrigo.mygateway>
2022-10-21 17:03:00 -0300

    lab-desig-system

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+    <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
+    
+    <button className='bg-cyan-500 font-medium px-4 py-1 rounded text-white'>
+      Enviar 
+    </button>
+    
+    </>
+
+  )
+}
+
